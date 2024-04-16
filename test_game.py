2abe2028a19b8ea48bee17999f27727e3224b0e9
Zatt010 <afuheho1@gmail.com>
2024-04-16 16:08:30 -0400

    Pruebas de los cambios del resultado para ambos jugadores

--- test_game.py
+++ test_game.py
@@ -8,7 +8,6 @@ class TestGameLogic(unittest.TestCase):
         # Configurar el entorno de prueba inicial
         self.win, self.paddle_left, self.paddle_right, self.ball, self.pen = game_logic.initialize_game()
 
-
     def test_move_paddle_right_up(self):
         # Simular movimiento hacia arriba de la paleta derecha
         initial_y = self.paddle_right.ycor()
@@ -20,6 +19,14 @@ class TestGameLogic(unittest.TestCase):
         initial_y = self.paddle_right.ycor()
         game_logic.move_paddle_right_down(self.paddle_right)
         self.assertEqual(self.paddle_right.ycor(), initial_y - 20)
+
+    def test_score_update(self):
+        # Simular incremento de puntuación y verificar la actualización del marcador
+        game_logic.player_a_score = 0
+        game_logic.player_b_score = 0
+        game_logic.game_loop(self.win, self.paddle_left, self.paddle_right, self.ball, self.pen)
+        self.assertEqual(game_logic.player_a_score, 1)
+        self.assertEqual(game_logic.player_b_score, 1)
 
     def tearDown(self):
         # Limpiar después de las pruebas
