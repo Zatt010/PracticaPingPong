e468f8254f2b11bb9c02ba84a42f02ded202bdd2
Jose Manuel Sanchez <99229719+JoseMeister-S@users.noreply.github.com>
2024-04-16 17:13:34 -0400

    Left paddle down

--- test_game.py
+++ test_game.py
@@ -26,6 +26,12 @@ class TestGameLogic(unittest.TestCase):
         game_logic.move_paddle_right_down(self.paddle_right)
         self.assertEqual(self.paddle_right.ycor(), initial_y - 20)
 
+    def test_move_paddle_left_down(self):
+        # Simular movimiento hacia abajo de la paleta izquierda
+        initial_y = self.paddle_left.ycor()
+        game_logic.move_paddle_left_down(self.paddle_left)
+        self.assertEqual(self.paddle_left.ycor(), initial_y - 20)
+
     def test_score_update(self):
         # Simular incremento de puntuación y verificar la actualización del marcador
         game_logic.player_a_score = 0
